911acaecac02afa0cc6becd9286c949ec61edd27
Алексей Киринюк <alexey.kirinuk@yandex.ru>
2017-05-13 05:31:24 +0700

    Написал защиту к 5 лабе (DequeVector/Protection.cpp).

--- University/Tasks/Solution/Labs/Plus/DequeVector/Protection.cpp
+++ University/Tasks/Solution/Labs/Plus/DequeVector/Protection.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 #include <list>
 #include <iterator>
+#include <set>
 
 using namespace std;
 
 class List
 {
 public:
-	List* next;
+	List* Next;
 	int Value;
 
 	List(int val)
 	{
 		Value = val;
-		next = nullptr;
+		Next = nullptr;
 	}
 	~List()
 	{
-		delete next;
+		delete Next;
 	}
 
 	void Add(int val)
 	{
 		List* element = this;
 
-		while (element->next != nullptr) 
+		while (element->Next != nullptr) 
 		{ 
-			element = element->next;
+			element = element->Next;
 		}
 
-		element->next = new List(val);
+		element->Next = new List(val);
 	}
 };
 void change(List* l)
@@ -37,17 +38,16 @@ void change(List* l)
 	List* n = l;
 	//List* next = n->next;
 
-	while(n != nullptr && n->next != nullptr)
+	while(n != nullptr && n->Next != nullptr)
 	{
-		List * temp = n->next->next;
-		List * nextIteration = n->next->next;
-		n->next->next = n;
-		n->next = temp == nullptr ? temp : temp->next != nullptr ? temp->next : temp;
+		List * temp = n->Next->Next;
+		List * nextIteration = n->Next->Next;
+		n->Next->Next = n;
+		n->Next = temp == nullptr ? temp : temp->Next != nullptr ? temp->Next : temp;
 		n = nextIteration;
 	}
 }
-
-int main()
+void TestChange()
 {
 	List *list = new List(1);
 	list->Add(2);
@@ -56,15 +56,15 @@ int main()
 	list->Add(5);
 	list->Add(6);
 
-	
+
 	List *first = list;
 	List *show = list;
-	List *second = list->next;
+	List *second = list->Next;
 
 	while (list != nullptr)
 	{
 		cout << list->Value << endl;
-		list = list->next;
+		list = list->Next;
 	}
 
 	change(first);
@@ -72,9 +72,57 @@ int main()
 	while (second != nullptr)
 	{
 		cout << second->Value << endl;
-		second = second->next;
+		second = second->Next;
 	}
-	
+
 	delete list;
+}
+
+int SumList(List* list, set<int> &set)
+{
+	List* element = list;
+	int result = 0;
+
+	while (element != nullptr)
+	{
+		if (set.find(element->Value) != set.end())
+		{
+			result += element->Value;
+		}
+		
+		element = element->Next;
+	}
+
+	return result;
+}
+void TestSum()
+{
+	List* list = new List(0);
+
+	for (int i = 1; i <= 10; i++)
+	{
+		list->Add(i);
+	}
+
+	set<int> set;
+
+	for (int i = -5; i <= 5; i++)
+	{
+		set.insert(i);
+	}
+
+	int sum = SumList(list, set);
+
+	cout << "Sum: 15 == " << sum << endl;
+
+	// 15
+	delete list;
+}
+
+int main()
+{
+	//TestChange();
+	TestSum();
+
 	system("pause");
 }
